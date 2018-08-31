fc332041dcd7ef33f3daf6f7913acbb1fb1a6bb5
MATTHEW SIMMONS <7671830@student.swin.edu.au>
2018-08-31 13:08:39 +1000

    Added a screen to add sales items
    The system needs lots of work

---
+++ SalesApp/AddSale.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SalesApp
+{
+    partial class AddSale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.sales_data = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.programBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.programBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.MainLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sales_data)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainLayout
+            // 
+            this.MainLayout.ColumnCount = 3;
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.MainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.MainLayout.Controls.Add(this.sales_data, 1, 1);
+            this.MainLayout.Controls.Add(this.button1, 1, 2);
+            this.MainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainLayout.Location = new System.Drawing.Point(0, 0);
+            this.MainLayout.Name = "MainLayout";
+            this.MainLayout.RowCount = 3;
+            this.MainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.7438F));
+            this.MainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.2562F));
+            this.MainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.MainLayout.Size = new System.Drawing.Size(784, 561);
+            this.MainLayout.TabIndex = 0;
+            // 
+            // sales_data
+            // 
+            this.sales_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sales_data.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sales_data.Location = new System.Drawing.Point(81, 55);
+            this.sales_data.Name = "sales_data";
+            this.sales_data.Size = new System.Drawing.Size(542, 426);
+            this.sales_data.TabIndex = 0;
+            this.sales_data.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_changed);
+            this.sales_data.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.data_row_added);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(81, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(542, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Finalise Sale";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.finalise_sale);
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(SalesApp.Program);
+            // 
+            // programBindingSource1
+            // 
+            this.programBindingSource1.DataSource = typeof(SalesApp.Program);
+            // 
+            // programBindingSource2
+            // 
+            this.programBindingSource2.DataSource = typeof(SalesApp.Program);
+            // 
+            // AddSale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.MainLayout);
+            this.Name = "AddSale";
+            this.Text = "Add Sale";
+            this.MainLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sales_data)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel MainLayout;
+        private System.Windows.Forms.DataGridView sales_data;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.BindingSource programBindingSource1;
+        private System.Windows.Forms.BindingSource programBindingSource2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SalesApp/AddSale.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesApp
+{
+    public partial class AddSale : Form
+    {
+
+        public AddSale()
+        {
+            InitializeComponent();
+
+            DataTable dt = new DataTable();
+            sales_data.DataSource = dt;
+
+            dt.Columns.Add("ItemNo");
+            dt.Columns.Add("Description");
+            dt.Columns.Add("Item Price");
+            dt.Columns.Add("Qty");
+            dt.Columns.Add("SubPrice");
+
+            sales_data.Columns["Description"].ReadOnly = true;
+            sales_data.Columns["Item Price"].ReadOnly = true;
+            sales_data.Columns["SubPrice"].ReadOnly = true;
+
+            sales_data.Columns["ItemNo"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            sales_data.Columns["Description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            sales_data.Columns["Item Price"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            sales_data.Columns["Qty"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            sales_data.Columns["SubPrice"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+
+        private void data_changed(object sender, DataGridViewCellEventArgs e)
+        {
+            DataTable dt = sales_data.DataSource as DataTable;
+            update_row_info(sales_data.DataSource as DataTable, e.RowIndex);
+        }
+
+        private void data_row_added(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            update_row_info(sales_data.DataSource as DataTable, e.RowIndex);
+        }
+
+        private void update_row_info(DataTable dt, int row)
+        {
+            // TODO: This doesn't update properly when a row is added because the DataTable doesn't get the row added until after the event
+            if (row < dt.Rows.Count)
+            {
+                string itemno = dt.Rows[row]["ItemNo"].ToString();
+                string name = ItemManager.GetItemName(itemno);
+                float price = ItemManager.GetItemPrice(itemno);
+                dt.Rows[row]["Description"] = name;
+                dt.Rows[row]["Item Price"] = price;
+
+                int qty;
+                try {
+                    qty = Int32.Parse(dt.Rows[row]["Qty"].ToString());
+                } catch (FormatException e)
+                {
+                    qty = 1;
+                    dt.Rows[row]["Qty"] = qty;
+                }
+                float subPrice = price * qty;
+                dt.Rows[row]["SubPrice"] = subPrice;
+            }
+        }
+
+        private void finalise_sale(object sender, EventArgs e)
+        {
+            DataTable dt = sales_data.DataSource as DataTable;
+            // TODO save the data to a database
+        }
+    }
+}
