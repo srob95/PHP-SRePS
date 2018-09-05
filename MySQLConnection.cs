98e692f42ec676fade5424e453fa46377695027f
robanderson1 <42598339+robanderson1@users.noreply.github.com>
2018-09-05 16:12:27 +1000

    MySQL connection
    Need to build on the SQL commands within the script but its connecting OK from whitelisted IP's

---
+++ MySQLConnection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DBConnect
+{
+    static void Main(string[] args)[]
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+
+    //Constructor
+    public DBConnect()
+    {
+        Initialize();
+    }
+
+    //Initialize values
+    private void Initialize()
+    {
+        server = "sql174.main-hosting.eu";
+        database = "u844012350_rambs";
+        uid = "u844012350_rambs";
+        password = "a123456789";
+        string connectionString;
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        connection = new MySqlConnection(connectionString);
+    }
+
+    //open connection to database
+    private bool OpenConnection()
+    {
+    }
+
+    //Close connection
+    private bool CloseConnection()
+    {
+    }
+
+    //Insert statement
+    public void Insert()
+    {
+    }
+
+    //Update statement
+    public void Update()
+    {
+    }
+
+    //Delete statement
+    public void Delete()
+    {
+    }
+
+    //Select statement
+    public List<string>[] Select()
+    {
+    }
+
+    //Count statement
+    public int Count()
+    {
+    }
+
+    //Backup
+    public void Backup()
+    {
+    }
+
+    //Restore
+    public void Restore()
+    {
+    }
+}
