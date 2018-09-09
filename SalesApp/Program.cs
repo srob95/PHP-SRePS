64d2f5464adb304daa00c5249645a2d07ef9f2c8
mdsimmo <mdsimmo2+git@gmail.com>
2018-09-10 09:54:35 +1000

    Added a local SQLite database
    The database is completely standalone. It does not require anything to
    be installed or configured. It runs completely from a dll file living in
    the project folder.
    
    I am currently just putting random junk into the database to prove it
    works. The busness logic of the app will need to be added.

--- SalesApp/Program.cs
+++ SalesApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace SalesApp
 {
@@ -11,9 +12,45 @@ namespace SalesApp
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AddSale());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new AddSale());
+
+            testSQL();
+        }
+
+        static void testSQL()
+        {
+            // Some random sql stuff to test functionality
+
+
+            // Create (or overwite) a database
+            // the file is stored in "PHP-SRePS\SalesApp\bin\Debug"
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+
+            SQLiteConnection dbConn = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3");
+            dbConn.Open();
+
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConn);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 3000)";
+            command = new SQLiteCommand(sql, dbConn);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, dbConn);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, dbConn);
+            command.ExecuteNonQuery();
+
+            sql = "select * from highscores order by score desc";
+            command = new SQLiteCommand(sql, dbConn);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
         }
     }
 }
